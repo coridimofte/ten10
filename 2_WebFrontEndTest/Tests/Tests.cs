2f35c82855f637ff973090475eb004cde534e729
coridimofte <c5iat6eA>
2018-02-04 02:27:03 +0000

    added first search part for  problem no 2
    added first search part for  problem no 2

---
+++ 2_WebFrontEndTest/Tests/Tests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using _2_WebFrontEndTest.UIFramework;
+using System.IO;
+
+namespace _2_WebFrontEndTest.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        internal IWebDriver driver;
+
+        [Test]
+        public void MyTest([Values("bmw")]String searchText,[Values("English")] String language)
+        {
+            driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+
+            var homePage = new HomePage(driver);
+            homePage.SearchFor(searchText, language);
+        }
+
+
+        /// <summary>
+        /// Test case setup; it is run for each test
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            Console.Out.WriteLine($"-----------{TestContext.CurrentContext.Test.Properties["Description"]}-----------");
+
+            driver = GetDriver();
+            driver.Manage().Window.Maximize();
+            //_driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(2));
+            //_driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(60));
+
+        }
+
+        /// <summary>
+        /// Test case "clean up"; it is run after each test
+        /// </summary>
+        [TearDown]
+        public void Close()
+        {
+           driver.Quit();
+        }
+
+        /// <summary>
+        /// Returns an instance of Chrome driver
+        /// </summary>
+        /// <returns></returns>
+        public IWebDriver GetDriver()
+        {
+            var opts = new ChromeOptions();
+            opts.AddArgument("test-type");
+            return new ChromeDriver(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), opts);
+        }
+    }
+}
---
+++ 2_WebFrontEndTest/UIFramework/HomePage.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_WebFrontEndTest.UIFramework
+{
+    public class HomePage
+    {
+        internal IWebDriver _driver;
+
+        [FindsBy(How=How.Id, Using = "searchInput")]
+        public IWebElement SearchInput;
+
+        public SelectElement Language => new SelectElement(_driver.FindElement(By.Id("searchLanguage")));
+
+        [FindsBy(How=How.XPath,Using =".//button[@type='submit']")]
+        public IWebElement SearchBtn;
+
+        public HomePage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(_driver, this);
+        }
+
+        /// <summary>
+        /// Searches for given text and language
+        /// </summary>
+        /// <param name="searchText"></param>
+        /// <param name="language"></param>
+        public void SearchFor(String searchText, String language="English")
+        {
+            SearchInput.SendKeys(searchText);
+            Language.SelectByText(language);
+            SearchBtn.Click();
+        }
+    }
+}
