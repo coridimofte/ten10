3f0f500993841d8730b2ceba18ccea335d845fca
coridimofte <c5iat6eA>
2018-02-05 23:34:40 +0000

    added the check for the given language and then for English

--- 2_WebFrontEndTest/Tests/Tests.cs
+++ 2_WebFrontEndTest/Tests/Tests.cs
@@ -17,15 +17,45 @@ namespace _2_WebFrontEndTest.Tests
         internal IWebDriver driver;
 
         [Test]
-        public void MyTest([Values("bmw")]String searchText,[Values("English")] String language)
+        public void MyTest([Values("bmw")]String searchText,[Values("Deutsch")] String language)
         {
+            //1. Navigate to the Wikipedia home page, http://www.wikipedia.org/. 
             driver.Navigate().GoToUrl("https://www.wikipedia.org/");
 
+            //2.Search for a given string in English: 
             var homePage = new HomePage(driver);
-            homePage.SearchFor(searchText, language);
+            homePage.SearchFor(searchText);
+
+            //3.Validate that the first heading of the search results page matches 
+            //the search string(ignoring case).
+            var searchResultPage = new SearchResultPage(driver);
+            var actual = searchResultPage.GetFirstHeading().ToLower().Contains(searchText.ToLower());
+            Checkpoint(true, actual, "The first heading does not match the search string.", "The first heading matches the string.");
+
+            //4. Verify that the search results page has link in another language
+            //defined as a parameter.
+            Checkpoint(true, searchResultPage.LanguageExists(language), "The language is not present", "The language is present");
+
+            //5. Navigate to the search results page in the other language.
+            searchResultPage.NavigateToLanguage(language);
+
+            //6.Validate that the search results page in the other language includes 
+            //a link to the version in English.
+            Checkpoint(true, searchResultPage.LanguageExists("English"), "The language is not present", "The language is present");
         }
 
-
+        /// <summary>
+        /// Checkpoint method
+        /// </summary>
+        /// <param name="expected"></param>
+        /// <param name="current"></param>
+        /// <param name="errMsg"></param>
+        /// <param name="successMsg"></param>
+        public void Checkpoint(bool expected, bool current, String errMsg, String successMsg)
+        {
+            Assert.AreEqual(expected, current, errMsg);
+            Console.Out.WriteLine("----Checkpoint passed---- :" + successMsg); ;
+        }
         /// <summary>
         /// Test case setup; it is run for each test
         /// </summary>
---
+++ 2_WebFrontEndTest/UIFramework/SearchResultPage.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_WebFrontEndTest.UIFramework
+{
+    public class SearchResultPage
+    {
+        internal IWebDriver _driver;
+
+        [FindsBy(How=How.Id, Using ="firstHeading")]
+        public IWebElement FirstHeading;
+
+        [FindsBy(How=How.XPath,Using= ".//*[@id='p-lang']/.//li/a")]
+        public IList<IWebElement> AvailableLangs;
+
+        public SearchResultPage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(_driver, this);
+        }
+
+        /// <summary>
+        /// Returns the first heading text
+        /// </summary>
+        /// <returns></returns>
+        public String GetFirstHeading()
+        {
+            return FirstHeading.Text;
+        }
+
+
+        /// <summary>
+        /// Returs true if given language is available
+        /// </summary>
+        /// <param name="lang"></param>
+        /// <returns></returns>
+        public bool LanguageExists(String lang)
+        {
+            return AvailableLangs.Any(l => l.Text == lang);
+        }
+
+        /// <summary>
+        /// Navigates to search results page in given language
+        /// </summary>
+        /// <param name="lang"></param>
+        public void NavigateToLanguage(String lang)
+        {
+            AvailableLangs.Where(l => l.Text == lang).First().Click();
+        }
+
+
+    }
+}
